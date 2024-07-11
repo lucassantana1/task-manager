7155ecc9f89cac27aa594afb567aec3acc9c7177
Lucas Santana <lucasdeoliveirasantana874@gmail.com>
2024-07-10 21:34:33 -0300

    feat: add route to create a new task

--- index.js
+++ index.js
@@ -6,12 +6,29 @@ const TaskModel = require("./src/models/task.model");
 
 dotenv.config();
 const app = express();
+app.use(express.json());
 
 connectToDatabase();
 
 app.get("/tasks", async (req, res) => {
-    const tasks = await TaskModel.find({});
-    res.status(200).send(tasks);
+    try {
+        const tasks = await TaskModel.find({});
+        res.status(200).send(tasks);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+app.post("/tasks", async (req, res) => {
+    try {
+        const newTask = new TaskModel(req.body);
+
+        await newTask.save();
+
+        res.status(201).send(newTask);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
 });
 
 app.listen(8000, () => console.log("listening on port 8000"));
